8f23f766b2cec4525d96fb3674bda8d51e04f357
Jiří Zahradil <jz@zahradil.info>
2020-04-23 13:45:12 +0200

    installation script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python3 -m venv venv
+. venv/bin/activate
+
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+pip install *.whl
+
+ln -s $(realpath ./jzhb) ~/bin/jzhbck
+
+echo -e "Skript [jzhb] installed to ~/bin ."
+echo -e "Hotovo."
